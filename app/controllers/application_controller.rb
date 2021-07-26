8a42830cc4294719ce297fd9475522119da6d36f
yukarikariri <doggieholic3015@gmail.com>
2021-07-26 17:27:24 +0900

    カレンダー表示機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    weight_path(current_user.id)
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :target_weight, :target_water])
