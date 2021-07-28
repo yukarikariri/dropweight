2df1f3b76d4c0cfc746d045407fbf208d049e9e7
yukarikariri <doggieholic3015@gmail.com>
2021-07-28 09:25:44 +0900

    simple calendar導入後の細かな調整

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2021_07_24_004018) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "target_weight", null: false
-    t.integer "target_water"
+    t.float "target_weight", null: false
+    t.float "target_water", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
